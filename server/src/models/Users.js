c2b5f9331691b59c58c12e6a82eb16b5a3d85d5a
fajarrw <frizkywidodo@gmail.com>
2023-03-26 14:46:25 +0700

    add journal model and hournal route

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose"
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true},
     password: { type: String, required: true },
+    name: { type: String },
+    learningStyle: { type: Number }
 });
 
 export const UserModel = mongoose.model("users", UserSchema)
